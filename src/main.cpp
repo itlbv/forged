06eb21e30bc6d226f5ef8aef8fcdd2279e51d207
a.fokin <a.fokin@sportradar.com>
2020-10-16 15:31:16 +0200

    Fix SDL_INIT_VIDEO call in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
         std::cout << "SDL_Init FAILED. Error: " << SDL_GetError() << std::endl;
 
     RenderWindow renderWindow("Forged", 800, 600);
