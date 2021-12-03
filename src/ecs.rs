2b8fe565c7a395757af18fd48d0c17fb38e35132
Artem Fokin <artem@favrit.com>
2021-12-03 09:02:05 +0100

    Add Ecs prototype

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,61 +1,98 @@
-pub struct NameComponent(pub &'static str);
+use std::cell::RefCell;
 
-pub struct HealthComponent(pub i32);
-impl HealthComponent {
-    fn decrease(&mut self) {
-        self.0 -= 1;
+trait ComponentVec {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+    fn push_none(&mut self);
+}
+
+impl<T> ComponentVec for RefCell<Vec<Option<T>>> {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self as &dyn std::any::Any
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self as &mut dyn std::any::Any
+    }
+
+    fn push_none(&mut self) {
+        self.get_mut().push(None);
     }
 }
 
-pub struct World {
-    entity_count: usize,
-    name_comps: Vec<Option<NameComponent>>,
-    health_comps: Vec<Option<HealthComponent>>,
+struct Ecs {
+    entities_count: usize,
+    component_vecs: Vec<Box<dyn ComponentVec>>,
 }
-impl World {
-    pub fn new() -> Self {
+
+impl Ecs {
+    fn new() -> Self {
         Self {
-            entity_count: 0,
-            name_comps: Vec::new(),
-            health_comps: Vec::new(),
+            entities_count: 0,
+            component_vecs: Vec::new(),
         }
     }
 
-    pub fn new_entity(&mut self) -> usize {
-        self.name_comps.push(None);
-        self.health_comps.push(None);
-        self.entity_count += 1;
-        self.entity_count
+    fn new_entity(&mut self) -> usize {
+        let entity_id = self.entities_count;
+        for component_vec in self.component_vecs.iter_mut() {
+            component_vec.push_none();
+        }
+        self.entities_count += 1;
+        entity_id
     }
 
-    pub fn add_components_to_entity(
+    fn add_component_to_entity<ComponentType: 'static>(
         &mut self,
-        name_comp: NameComponent,
-        health_comp: HealthComponent,
         entity: usize,
+        component: ComponentType,
     ) {
-        self.name_comps[entity - 1] = Some(name_comp);
-        self.health_comps[entity - 1] = Some(health_comp);
-    }
-}
-
-pub fn hunger_system(world: &mut World) {
-    let comps = world
-        .health_comps
-        .iter_mut()
-        .zip(world.name_comps.iter())
-        .filter_map(
-            |(health, name): (&mut Option<HealthComponent>, &Option<NameComponent>)| {
-                Some((health.as_mut()?, name.as_ref()?))
-            },
-        );
-    for (health, name) in comps {
-        health.decrease();
-        if health.0 <= 0 {
-            println!("{} died of hunger!", name.0);
-        } else {
-            println!("{} is getting hungry!", name.0);
+        for component_vec in self.component_vecs.iter_mut() {
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>() {
+                component_vec.get_mut[entity] = Some(component);
+                return;
+            }
         }
+
+        // No matching component storage exists yet, so we have to make one.
+        let mut new_component_vec: Vec<Option<ComponentType>> =
+            Vec::with_capacity(self.entities_count);
+
+        // All existing entities don't have this component, so we give them `None`
+        for _ in 0..self.entities_count {
+            new_component_vec.push(None);
+        }
+
+        // Give this Entity the Component.
+        new_component_vec[entity] = Some(component);
+        self.component_vecs.push(Box::new(RefCell::new(new_component_vec)));
+    }
+
+    fn borrow_component_vec<ComponentType: 'static>(&self) -> Option<RefMut<Vec<Option<ComponentType>>>> {
+        for component_vec in self.component_vecs.iter() {
+            if let Some(component_vec) = component_vec
+                .as_any()
+                .downcast_ref::<Vec<Option<ComponentType>>>()
+            {
+                return Some(component_vec.borrow_mut());
+            }
+        }
+        None
     }
 }
 
+/*
+let mut healths = world.borrow_component_vec_mut::<Health>().unwrap();
+let mut names = world.borrow_component_vec_mut::<Name>().unwrap();
+let zip = healths.iter_mut().zip(names.iter_mut());
+let iter = zip.filter_map(|(health, name)| Some((health.as_mut()?, name.as_mut()?)));
+
+for (health, name) in iter
+{
+    if name.0 == "Perseus" && health.0 <= 0 {
+        *health = Health(100);
+    }
+}
+ */
