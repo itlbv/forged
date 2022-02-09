8a32340fb13fefe5cf625304db56ca617c102088
Artem Fokin <artem@favrit.com>
2022-02-09 12:31:25 +0300

    Rewrite Ecs prototype

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,61 +1,45 @@
-pub struct NameComponent(pub &'static str);
+use std::any::{Any, TypeId};
+use std::borrow::BorrowMut;
+use std::collections::HashMap;
 
-pub struct HealthComponent(pub i32);
-impl HealthComponent {
-    fn decrease(&mut self) {
-        self.0 -= 1;
-    }
+type EntityIdx = usize;
+
+pub trait Component {}
+
+struct ComponentVec {
+    vec: Vec<Option<Box<dyn Component>>>,
 }
 
-pub struct World {
-    entity_count: usize,
-    name_comps: Vec<Option<NameComponent>>,
-    health_comps: Vec<Option<HealthComponent>>,
+pub struct Ecs {
+    entity_count: EntityIdx,
+    comps_registry: HashMap<TypeId, ComponentVec>,
 }
-impl World {
+
+impl Ecs {
     pub fn new() -> Self {
         Self {
             entity_count: 0,
-            name_comps: Vec::new(),
-            health_comps: Vec::new(),
+            comps_registry: Default::default(),
         }
     }
 
-    pub fn new_entity(&mut self) -> usize {
-        self.name_comps.push(None);
-        self.health_comps.push(None);
+    pub fn register_component<C: Component + 'static>(&mut self) {
+        self.comps_registry.insert(TypeId::of::<C>(), ComponentVec{vec: vec![]});
+    }
+
+    pub fn create_entity(&mut self) -> EntityIdx {
         self.entity_count += 1;
+        for (_, comp_vec) in self.comps_registry.borrow_mut() {
+            comp_vec.vec.push(Option::None);
+        }
         self.entity_count
     }
 
-    pub fn add_components_to_entity(
-        &mut self,
-        name_comp: NameComponent,
-        health_comp: HealthComponent,
-        entity: usize,
-    ) {
-        self.name_comps[entity - 1] = Some(name_comp);
-        self.health_comps[entity - 1] = Some(health_comp);
+    pub fn add_component_to_entity<C: Component + 'static>(&mut self, entity_idx: EntityIdx, comp: C) {
+        self.comps_registry
+            .get_mut(&comp.type_id())
+            .unwrap()
+            .vec
+            .insert(entity_idx, Option::Some(Box::new(comp)));
     }
 }
-
-pub fn hunger_system(world: &mut World) {
-    let comps = world
-        .health_comps
-        .iter_mut()
-        .zip(world.name_comps.iter())
-        .filter_map(
-            |(health, name): (&mut Option<HealthComponent>, &Option<NameComponent>)| {
-                Some((health.as_mut()?, name.as_ref()?))
-            },
-        );
-    for (health, name) in comps {
-        health.decrease();
-        if health.0 <= 0 {
-            println!("{} died of hunger!", name.0);
-        } else {
-            println!("{} is getting hungry!", name.0);
-        }
-    }
-}
-
