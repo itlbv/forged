ef7d2156713e271ea58f359aff7119d74991f2ba
Artem Fokin <artem@favrit.com>
2022-03-07 09:48:48 +0100

    Add traversing tiles circling from the start position algorithm

--- src/util.rs
+++ src/util.rs
@@ -6,8 +6,22 @@ pub fn find_free_tiles(start_tile_x: i32, start_tile_y: i32, w: i32, h: i32, mar
     let map_nodes = map.borrow_tiles();
     let node = map_nodes.borrow_node(5, 8);
     println!("{:?}", node);
+    for node in map_nodes.iterator() {}
 
-    for node in map_nodes.iterator() {
+    for i in 1..=5 {
+        // node (-x, -y..y)
+        // node (x, -y..y)
+        for y in 0 - i..=i {
+            println!("{}, {}", i, y);
+            println!("{}, {}", -i, y);
+        }
+        // node (-x..x, -y)
+        // node (-x..x, y)
+        for x in 0 - i..=i {
+            println!("{}, {}", x, i);
+            println!("{}, {}", x, -i);
+        }
     }
+
     (5, 7)
 }
