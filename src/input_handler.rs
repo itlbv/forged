76c58f96e35ed16d175184ae4b3304be3b21ab17
Artem Fokin <artem@favrit.com>
2022-03-27 19:09:06 +0200

    Tweak zoom level adjustment

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -24,8 +24,8 @@ impl InputHandler {
                 Event::KeyDown { keycode: Some(Keycode::S), .. } => {properties.camera_y -= 20}
                 Event::KeyDown { keycode: Some(Keycode::A), .. } => {properties.camera_x += 20}
                 Event::KeyDown { keycode: Some(Keycode::D), .. } => {properties.camera_x -= 20}
-                Event::KeyDown { keycode: Some(Keycode::Z), .. } => {properties.zoom_factor -= 10}
-                Event::KeyDown { keycode: Some(Keycode::X), .. } => {properties.zoom_factor += 10}
+                Event::KeyDown { keycode: Some(Keycode::Z), .. } => {properties.zoom_factor -= 5}
+                Event::KeyDown { keycode: Some(Keycode::X), .. } => {properties.zoom_factor += 5}
                 _ => {}
             }
         }
