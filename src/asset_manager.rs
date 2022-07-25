bec5655c6de576276950a94456d97de1a921f3b8
Artem Fokin <artem@favrit.com>
2022-07-25 13:53:21 +0200

    Remove ttf_context struct field in AssetManager

--- src/asset_manager.rs
+++ src/asset_manager.rs
@@ -9,16 +9,14 @@ use sdl2::video::WindowContext;
 pub struct AssetManager<'l> {
     texture_creator: &'l TextureCreator<WindowContext>,
     textures: HashMap<String, Texture<'l>>,
-
-    ttf_context: &'l Sdl2TtfContext,
     font: Font<'l, 'l>,
 }
 
 impl<'l> AssetManager<'l> {
     pub fn new(texture_creator: &'l TextureCreator<WindowContext>, ttf_context: &'l Sdl2TtfContext) -> Self {
-        Self { texture_creator,
+        Self {
+            texture_creator,
             textures: HashMap::new(),
-            ttf_context,
             font: ttf_context.load_font(Path::new("assets/clacon2.ttf"), 24).unwrap(),
         }
     }
